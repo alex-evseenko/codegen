27233d59eba4ef3f72644dce46fe3f89461d2db7
AlexE <alex.evseenko@gmail.com>
2015-05-13 23:06:10 +0300

    Import declarations added.

--- test/com/adal/codegen/test/CodegenSpec.scala
+++ test/com/adal/codegen/test/CodegenSpec.scala
@@ -21,13 +21,12 @@ import org.specs2.runner.JUnitRunner
 class CodegenSpec extends SpecificationWithJUnit { def is = s2"""
   Specification of code generation:
 
-  class contains imports list 	$importsCheck
   class apply its default constructor ${~Class('adal, 'Test)() === "new Test()"}
   class contains methods list 	$methodsCheck
 
+  class contains imports list 	$importsCheck
   import's type defined         ${Import("com.foo.Bar")() === "Bar"}
   import's uniqueness           $importsUniqueness
-  imports generate properly     onItemClick.imports === s""import android.widget.AdapterView;Code.CRLFimport android.view.View;Code.CRLF""
 
   property is a class field     $propertyIsField
   property contains listeners   $propertyContainsListeners
@@ -76,6 +75,9 @@ $"""
     onItemClick.get += 'Code -> doRefresh(getId())
     onItemClick.get += 'Code -> doActivate(onItemClick.get.params(3))
   }
+  // add imports list from anonymous class
+  activity <~ onItemClickListener.importsList
+
   activity += onCreate
   // create the method implementation 
   onCreate += 'Code ->
@@ -126,14 +128,9 @@ public void onItemClick(AdapterView arg1, View arg2, int arg3, long arg4) {
     activity.holder.contains("import android.widget.TextView;") &&
     activity.holder.contains("import android.widget.ListView;") &&
     activity.holder.contains("import android.os.Bundle;") &&
-    activity.holder.contains("import android.content.Intent;") //&&
-//    activity.holder.contains("import android.widget.AdapterView;") //&&
-//    activity.holder.contains("import com.foo.Bar;") &&
-//    activity.holder.contains("import android.view.View;") &&
-//    activity.holder.contains("import android.widget.AdapterView;") &&
-//    activity.holder.contains("import android.app.Dialog;") &&
-//    activity.holder.contains("import java.util.Calendar;") &&
-//    activity.holder.contains("import android.app.DatePickerDialog;")
+    activity.holder.contains("import android.content.Intent;") &&
+    activity.holder.contains("import android.widget.AdapterView;") &&
+    activity.holder.contains("import android.view.View;")
 
 
   def importsUniqueness = {
